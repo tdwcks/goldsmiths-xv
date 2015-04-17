2a61d04a69d0591359793dfa4b417fca1b7b29e8
Tom Wicks <tom@miln.co>
2015-04-17 16:41:48 +0100

    New Styles Applied

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,34 +1,35 @@
 // Lazy Load
+$(document).ready(function () {
 
-$(function() {
     $("img.lazy").lazyload({
         event : "load_images"
     });
-});
 
 // Designer Expand 
 
-$(document).on('click', '.list-all-designers li', function(e) {
-	e.preventDefault(); 
-	var selected_designer = $(this).attr('name');
+	$(document).on('click', '.list-all-designers li', function(e) {
+		e.preventDefault(); 
+		var selected_designer = $(this).attr('name');
 
-	if ($('body').hasClass('project--expanded')) {
-		$('#' + selected_designer).removeClass('active--project--expand');
-		$('body').removeClass('project--expanded');
-		$('body').removeClass('menu--active');
-	}
+		if ($('body').hasClass('project--expanded')) {
+			$('#' + selected_designer).removeClass('active--project--expand');
+			$('body').removeClass('project--expanded');
+			$('body').removeClass('menu--active');
+		}
 
-	else {
-		$('body').addClass('active--project--expand');
-		$('#' + selected_designer).addClass('active--project--expand');
-		$('#' + selected_designer + " img.lazy").trigger("load_images")
-	}	
- })
+		else {
+			$('body').addClass('active--project--expand');
+			$('#' + selected_designer).addClass('active--project--expand');
+			$('#' + selected_designer + " img.lazy").trigger("load_images")
+		}	
+	})
 
 // Designer Close
 
-$(document).on('click', '.layout-designer-expanded .layout-one-column-grid .back-to-all', function(e) {
-	e.preventDefault(); 
-		$('.layout-project-expanded').removeClass('active--project--expand');
-		$('body').removeClass('menu--active');	
- })
+	$(document).on('click', '.layout-designer-expanded .layout-one-column-grid .back-to-all', function(e) {
+		e.preventDefault(); 
+			$('.layout-project-expanded').removeClass('active--project--expand');
+			$('body').removeClass('menu--active');	
+	})
+
+});

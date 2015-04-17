4fc25e173b03816a57b7f15c9bf037ac8b572f96
Tom Wicks <tom@miln.co>
2015-04-17 19:28:59 +0100

    Slider Issues

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,6 @@
 // Lazy Load
-$(document).ready(function () {
+
+$
 
     $("img.lazy").lazyload({
         event : "load_images"
@@ -14,11 +15,11 @@ $(document).ready(function () {
 		if ($('body').hasClass('project--expanded')) {
 			$('#' + selected_designer).removeClass('active--project--expand');
 			$('body').removeClass('project--expanded');
-			$('body').removeClass('menu--active');
 		}
 
 		else {
 			$('body').addClass('active--project--expand');
+			$('.layout-project-expanded').removeClass('active--project--collapse');
 			$('#' + selected_designer).addClass('active--project--expand');
 			$('#' + selected_designer + " img.lazy").trigger("load_images")
 		}	
@@ -26,10 +27,38 @@ $(document).ready(function () {
 
 // Designer Close
 
-	$(document).on('click', '.layout-designer-expanded .layout-one-column-grid .back-to-all', function(e) {
+	$(document).on('click', '.layout-designer-expanded .back-to-all', function(e) {
 		e.preventDefault(); 
+			$('.layout-project-expanded').addClass('active--project--collapse');
 			$('.layout-project-expanded').removeClass('active--project--expand');
-			$('body').removeClass('menu--active');	
+			$('.layout-project-expanded').removeClass('active--project--expand');
+			$('body').removeClass('active--project--expand');
 	})
 
+// Scroll To Class Menu Function 
+
+      function goToByScroll(id){
+          $('html,body').animate({
+              scrollTop: $("."+id).offset().top - 60}, 400);
+      }
+
+      $(document).on('click', '.menu-item-link', function(e) {
+          e.preventDefault(); 
+          goToByScroll($(this).attr("id"));
+          $('.menu-item-link').removeClass('menu--item--active');
+          $(this).addClass('menu--item--active');   
+      });
+
+
+/// Image Swiper
+
+
+$(document).ready(function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+	roundLengths: true,
+    loop: true,
+   	pagination: '.swiper-pagination',
+    paginationClickable: true
+    })
 });
+

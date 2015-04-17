ba1d557dcc1e96e766e78539cd725f7ec9b622d6
Tom Wicks <tom@miln.co>
2015-04-17 08:50:33 +0100

    Scroll Removed

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,27 +1,3 @@
-
-// Button Menu
-
-$(document).on('click', '.button-menu-trigger', function(e) {
-	e.preventDefault(); 
-
-	if ($('body').hasClass('menu--active')) {
-		$('body').removeClass('menu--active');
-		$('body').addClass('menu--collapsing');
-		$(".button-menu-trigger").html('Menu')
-	}
-	else {
-		$('body').removeClass('menu--collapsing');
-		$('body').addClass('menu--active');
-		$(".button-menu-trigger").html('Close')
-	}
- })
-
-// Click anywhere to close menu
-
-$('html').click(function() {
-	
-});
-
 // Lazy Load
 
 $(function() {
@@ -56,17 +32,3 @@ $(document).on('click', '.layout-designer-expanded .layout-one-column-grid .back
 		$('.layout-project-expanded').removeClass('active--project--expand');
 		$('body').removeClass('menu--active');	
  })
-
-// Scroll To Class Menu Function 
-
-function goToByScroll(id){
-    $('html,body').animate({
-        scrollTop: $("."+id).offset().top},
-        '800');
-}
-
-$(document).on('click', '.menu-item-link', function(e) {
-    e.preventDefault(); 
-    goToByScroll($(this).attr("id"));           
-});
-
